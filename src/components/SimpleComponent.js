dd3312c8d9f26b131d3254f4ed30fd81ee4e186e
BaronLip <baronlip@gmail.com>
2019-11-21 16:13:44 -0600

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,34 @@
-// Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            mood: "happy"
+        }
+    }
+
+    handleClick() {
+        console.log("Hit handleClick")
+        console.log(this)
+        if (this.state.mood === "happy") {
+            return this.setState({
+                mood: "sad"
+            })
+        } else if (this.state.mood === "sad") {
+            return this.setState({
+                mood: "happy"
+            })
+        }
+    }
+
+    render() {
+        console.log("Hit SimpleComponent")
+        console.log(this.state.mood)
+        return (
+            <div onClick={this.handleClick.bind(this)}>
+                <button>{this.state.mood}</button>
+            </div>
+        )
+    }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,26 @@
-// Code SimplerComponent Here
+import React, { Component } from "react";
+
+
+// With functional components there is no need for "this".
+const SimplerComponent = props => <div onClick={props.handleClick}> I am just happy.</div>
+
+export default SimplerComponent;
+
+
+// export default class SimplerComponent extends Component {
+
+    // changeText() {
+    //     const sentence = document.getElementById("sentence")
+    //     sentence.innerHTML = "I am just sad."
+    // }
+
+    // render() {
+    //     console.log("Hit SimplerComponent")
+    //     return (
+    //         <div onClick={this.changeText.bind(this)}>
+    //             <span id="sentence">"I am just happy."</span>
+    //         </div>
+    //     )
+    // }
+// }
+
